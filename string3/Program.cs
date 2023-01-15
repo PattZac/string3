6f89c217067553b2c2f8e352cabbf0c56c44ee89
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-15 14:21:06 +0100

    Add files via upload

---
+++ string3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string sVelika = s.ToUpper();
+            string sMala = s.ToLower();
+            string sPrvaTri = s.Substring(0, 3);
+            string sZadnjihPet = s.Substring(s.Length - 5, 5);
+            string s8_11 = s.Substring(8, 11);
+            Console.WriteLine(sVelika + "\r\n" + sMala + "\r\n" + sPrvaTri + "\r\n" + sZadnjihPet + "\r\n" + s8_11);
+            Console.ReadKey();
+        }
+    }
+}
